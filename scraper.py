3bc992bdd58c36f10a61e3bd5618ac145979d1d1
Dave Hall <dave@cyberdave.tech>
2021-04-15 12:07:59 -0700

    Update scraper.py
    Didn't upload the correct file originally, so had to fix the bugs.

--- scraper.py
+++ scraper.py
@@ -16,10 +16,10 @@ number_of_pages = int(input())
 type_of_article = input()
 if type_of_article == 'Research Highlight':
     type_of_class = 'article-item__body'
-elif type_of_article == 'News':
+elif type_of_article == 'News' or type_of_article == 'News Feature':
     type_of_class = 'article__body'
 
-for i in range(1, number_of_pages):
+for i in range(1, number_of_pages + 1):
     os.mkdir('Page_' + str(i))
     os.chdir('Page_' + str(i))
     r = requests.get(url + str(i), headers={ 'Accept-Language': 'en-US,en;q=0.5' })
@@ -30,15 +30,13 @@ for i in range(1, number_of_pages):
     links = []
     for link in article_titles:
         links.append(link.get('href'))
-    for i in range(len(article_types)):
-        if article_types[i].text.strip() == type_of_article:
-            new_name = article_titles[i].text.strip()
+    for j in range(len(article_types)):
+        if article_types[j].text.strip() == type_of_article:
+            new_name = article_titles[j].text.strip()
             article_name = new_name.translate(translator).replace(" ", "_") + ".txt"
             new_file = open(article_name, 'wb')
-            r1 = requests.get(link_url + links[i], headers={ 'Accept-Language': 'en-US,en;q=0.5' })
+            r1 = requests.get(link_url + links[j], headers={ 'Accept-Language': 'en-US,en;q=0.5' })
             soup1 = BeautifulSoup(r1.content, 'html.parser')
-            # print(soup1.find('div', class_ = type_of_class).text.strip())
-            #              soup1.find('div', class_='article__body').text.strip()
             new_file.write(soup1.find('div', class_= type_of_class).text.strip().encode())
             new_file.close()
     os.chdir('..')
